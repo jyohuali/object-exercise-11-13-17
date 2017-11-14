f61166dbb1ab73d52e2465b7f6cb046acf5752bb
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-13 19:10:36 -0500

    add js/object

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var car = {
+  isAppealing: true,
+  brand:["ferrary", "lamborgini", "audi","mclaren"],
+  horsePower: 400,
+
+  dream: function (name){
+    console.log( name + " would love to have a lamborgini")
+  }
+}
+car.dream("Jonathan");
